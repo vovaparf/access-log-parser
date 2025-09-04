38f61304e3ceae2f2e5ea500c5de7f77118b9b47
Parfenov Vladimir <vova_parf@mail.ru>
2025-09-04 13:43:46 +0300

    Разработан метод, который проверяет корретктный ли путь к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число");
-        int secondNumber = new Scanner(System.in).nextInt();
-        System.out.println("Сумма чисел: " + (firstNumber + secondNumber));
-        System.out.println("Разность чисел: " + (firstNumber - secondNumber));
-        System.out.println("Произведение чисел: " + (firstNumber * secondNumber));
-        System.out.println("Частное чисел: " + ((double) firstNumber / secondNumber));
+        Scanner sc = new Scanner(System.in);
+        int fileCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = sc.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный путь не существует или это папка. Попробуйте снова.");
+                continue;
+            }
+
+            fileCount++;
+            System.out.println("Путь указан верно!");
+            System.out.println("Это файл номер " + fileCount);
         }
     }
+}
